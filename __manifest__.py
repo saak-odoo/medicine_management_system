35165a3f9a672c62fcf54c131f36a13abc9b4cca
Aman Kumar <saak@odoo.com>
2023-08-16 11:35:48 +0530

    second_commit

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Medicine_Management_Sysytem",
+    'summary': "Medicine Management System",
+    'author': "Aman Kumar Sah",
+    'website': "https://www.odoo.com",
+    'category': 'Real Estate/Brokerage',
+    'version': '1.0',
+    'depends': ['base'],
+
+
+    # 'data': [
+        # 'security/ir.model.access.csv',
+    #     'views/estate_property_view.xml',
+    #     'views/estate_menus.xml',
+    #     'views/estate_property_List_view.xml',
+    #     'views/estate_property_offer_view.xml',
+    #     'views/estate_properrty_type_view.xml',
+    #     'views/estate_property_tag_view.xml',
+    # ],
+
+
+    # "demo": [
+    #     "demo/demo_data.xml",
+    # ]
+}
+
+
